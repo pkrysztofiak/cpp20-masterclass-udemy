f3afcab6541afb65132e5e7b9cdc3c5a36446b51
Przemek Krysztofiak <p.krysztofiak@protonmail.com>
2021-12-22 08:22:18 +0100

    First cpp program #1

---
+++ 3.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+consteval int get_value() {
+    return 3;
+}
+
+int main() {
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Number3" << std::endl;
+
+    std::cout << "Przemek Krysztofiak" << std::endl;
+    std::cout << "Przemek Krysztofiak" << std::endl;
+    std::cout << "Przemek Krysztofiak" << std::endl;
+    std::cout << "Przemek Krysztofiak" << std::endl;
+    std::cout << "Przemek Krysztofiak" << std::endl;
+    std::cout << "Przemek Krysztofiak" << std::endl;
+    std::cout << "Przemek Krysztofiak" << std::endl;
+    std::cout << "Przemek Krysztofiak" << std::endl;
+    std::cout << "Przemek Krysztofiak" << std::endl;
+    std::cout << "Przemek Krysztofiak" << std::endl;
+}
